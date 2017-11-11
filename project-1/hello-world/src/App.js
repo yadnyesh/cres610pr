06f3bbe7d3a3991e51aff09ce141e5def8abca48
Unknown <yadnyesh.juvekar@gmail.com>
2017-11-11 10:52:08 +0530

    Moved Header component into seperate file

--- project-1/hello-world/src/App.js
+++ project-1/hello-world/src/App.js
@@ -2,25 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './Header';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
         <Intro />
-      </div>
-    );
-  }
-}
-
-class Header extends Component {
-  render() {
-    return(
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
       </div>
     );
   }
