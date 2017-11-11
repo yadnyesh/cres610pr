8860fefddc4ed01335c3c89c437e9007e8f3479f
Unknown <yadnyesh.juvekar@gmail.com>
2017-11-11 10:31:33 +0530

    Created Components

--- project-1/hello-world/src/App.js
+++ project-1/hello-world/src/App.js
@@ -6,13 +6,33 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <Intro />
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return(
+      <div>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      </div>
+    );
+  }
+}
+
+class Intro extends Component {
+  render() {
+    return(
+      <div>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
       </div>
     );
   }
