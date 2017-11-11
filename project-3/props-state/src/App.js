cba0a832fe6a9a434b9c9ae1c7dc401ac490e3bb
Unknown <yadnyesh.juvekar@gmail.com>
2017-11-11 15:20:16 +0530

    Props from one Component to another

--- project-3/props-state/src/App.js
+++ project-3/props-state/src/App.js
@@ -13,9 +13,35 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Parent />
       </div>
     );
   }
 }
+
+class Parent extends Component {
+  render() {
+    return(
+      <div>
+        <h2>Just some info</h2>
+        <Cars msg="Cars are cool" model="34756"/>
+      </div>
+    );
+  }
+}
+
+class Cars extends Component {
+  render() {
+    console.log(this.props)
+    return(
+      <div>
+        <h3>I am from cars component</h3>
+        <p>{this.props.msg}</p>
+        <p>{this.props.model}</p>
+      </div>
+    );
+  }
+}
+
 
 export default App;
