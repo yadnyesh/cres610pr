53807d3dbddea0499f0f91438e31eab8a633c779
Unknown <yadnyesh.juvekar@gmail.com>
2017-11-11 17:03:57 +0530

    Created a function.

--- project-4/component-life-cycle/src/App.js
+++ project-4/component-life-cycle/src/App.js
@@ -10,18 +10,30 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Component Life Cycle</h1>
         </header>
-
+        <Body />
       </div>
     );
   }
 }
 
 
+
 class Body extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getRandomNumber = this.getRandomNumber.bind(this);
+  }
+  getRandomNumber(){
+    console.log("Random number generator");
+  };
   render() {
     return(
       <div>
-
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={this.getRandomNumber}>Random Number</button>
       </div>
     );
   }
