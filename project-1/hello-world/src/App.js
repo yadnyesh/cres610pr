e74ed9f598b125295d0d251efdfee803bd1bba6c
Unknown <yadnyesh.juvekar@gmail.com>
2017-11-11 10:53:22 +0530

    Moved Intro into different component

--- project-1/hello-world/src/App.js
+++ project-1/hello-world/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './Header';
+import Intro from './Intro';
 
 class App extends Component {
   render() {
@@ -10,18 +11,6 @@ class App extends Component {
       <div className="App">
         <Header />
         <Intro />
-      </div>
-    );
-  }
-}
-
-class Intro extends Component {
-  render() {
-    return(
-      <div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
       </div>
     );
   }
