3c5293cb7868dc35119a4b981a03c80c60843d2c
Unknown <yadnyesh.juvekar@gmail.com>
2017-11-18 10:14:55 +0530

    Completing Stop Watch component

--- project-6/full-scale-apps/stop-watch/src/App.js
+++ project-6/full-scale-apps/stop-watch/src/App.js
@@ -9,12 +9,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Timer App in React js</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Timer />
+        <Timer start={Date.now()}/>
       </div>
     );
   }
