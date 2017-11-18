0ceef993a2672c1bd729b064a423118f81e12f32
Unknown <yadnyesh.juvekar@gmail.com>
2017-11-18 10:34:56 +0530

    Created and passed in Array to sub component

--- project-6/full-scale-apps/course-purchase/src/App.js
+++ project-6/full-scale-apps/course-purchase/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Coursesales from './Coursesales';
 
 class App extends Component {
   render() {
+    var courses = [
+      {name: 'Complete iOS10 dev course', price: 199},
+      {name: 'Complete Pentesting course', price: 299},
+      {name: 'Complete Front End Dev dev course', price: 180},
+      {name: 'Big bounty Pen testing', price: 190}
+    ]
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Course Purchase page</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Coursesales items={courses}/>
       </div>
     );
   }
---
+++ project-6/full-scale-apps/course-purchase/src/Coursesales.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Coursesales extends Component {
+  render() {
+    console.log(this.props.items)
+    return(
+      <div>
+        I am from Course Sales page
+      </div>
+    );
+  }
+}
+
+export default Coursesales;
